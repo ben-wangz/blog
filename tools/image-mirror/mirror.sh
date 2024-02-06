88a4b1db5709a65c250d9b8a237fd8ef16d9d1d1
ben.wangz <ben.wangz@foxmail.com>
2024-02-06 17:45:43 +0800

    bugfix

--- tools/image-mirror/mirror.sh
+++ tools/image-mirror/mirror.sh
@@ -11,8 +11,8 @@ REPOSITORY_PREFIX=${REPOSITORY_PREFIX:-}
 IMAGE_YAML_FILE=${IMAGE_YAML_FILE:-${SCRIPT_DIR}/images.yaml}
 KEEP_STRUCTURE=${KEEP_STRUCTURE:-true}
 STRING_TO_REPLACE_SLASH=${STRING_TO_REPLACE_SLASH:-_}
-PUSH_WITH_TLS_VERIFY=$PUSH_WITH_TLS_VERIFY:-true}
-podman login --tls-verify=false -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} ${TARGET_REGISTRY}
+PUSH_WITH_TLS_VERIFY=${PUSH_WITH_TLS_VERIFY:-true}
+podman login --tls-verify=${PUSH_WITH_TLS_VERIFY} -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} ${TARGET_REGISTRY}
 yq eval '.[] | ("registry=" + .registry + "; repository=" + .repository + "; tag=" + .tag)'  ${IMAGE_YAML_FILE} \
   | while read -r EXPRESION
     do
