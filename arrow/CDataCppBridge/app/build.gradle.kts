560f7efc8d0c85d602f7465f87b506f5c65342aa
qipan2333 <qp_happyman@163.com>
2024-04-29 14:33:40 +0800

    apache arrow java-cpp demo

---
+++ arrow/CDataCppBridge/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+version = "0.1.0"
+
+val lombokDependency = "org.projectlombok:lombok:1.18.22"
+val jacksonVersion = "2.13.4"
+val slf4jVersion = "2.0.9"
+val arrowVersion = "15.0.2"
+dependencies {
+    implementation(project(":arrow:CDataCppBridge:CDataCppBridge"))
+    implementation("org.apache.arrow:arrow-c-data:$arrowVersion")
+    implementation("org.apache.arrow:arrow-vector:$arrowVersion")
+    implementation("org.apache.arrow:arrow-memory-core:$arrowVersion")
+    implementation("org.apache.arrow:arrow-memory-netty:$arrowVersion")
+    implementation("org.apache.arrow:arrow-format:$arrowVersion")
+    annotationProcessor(lombokDependency)
+    implementation("com.google.guava:guava:32.1.1-jre")
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+
+    shadow(lombokDependency)
+    shadow("org.slf4j:slf4j-simple:$slf4jVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "tech.geekcity.javacpp.app.Application"
+}
+
+tasks.shadowJar {
+    relocate("com.google.common", "tech.geekcity.flink.shadow.com.google.common")
+}
--- arrow/javacpp/README.md
+++ arrow/javacpp/README.md
@@ -3,8 +3,7 @@
 ## how to run
 ```shell
 ./gradlew :arrow:javacpp:app:shadowJar
-./gradlew :arrow:javacpp:geekmath:jar
-./gradlew :arrow:javacpp:geekmath:javacppJar
+./gradlew :arrow:javacpp:geekmath:customJar
 CURRENT_PATH=$(pwd)
-java -cp "$CURRENT_PATH/arrow/javacpp/app/build/libs/app-0.1.0-all.jar:$CURRENT_PATH/garrow/javacpp/geekmath/build/libs/geekmath-0.1.0.jar:$CURRENT_PATH/arrow/javacpp/geekmath/build/libs/geekmath-0.1.0-linux-x86_64.jar" tech.geekcity.javacpp.app.Application
+java -cp "$CURRENT_PATH/arrow/javacpp/app/build/libs/app-0.1.0-all.jar:$CURRENT_PATH/arrow/javacpp/geekmath/build/libs/geekmath-0.1.0.jar" tech.geekcity.javacpp.app.Application
 ```
