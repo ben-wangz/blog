bdfd838eec17fc95f95854adad64d6fc512ed5c5
ben.wangz <ben.wangz@foxmail.com>
2024-06-12 04:00:59 +0000

    with privileged

--- docs/src/articles/kubernetes/argocd/argo-workflows/publish-container-image/container/build-in-container-test.sh
+++ docs/src/articles/kubernetes/argocd/argo-workflows/publish-container-image/container/build-in-container-test.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 podman run --rm \
     -v $SCRIPT_DIR:/code \
+    --privileged \
     -it m.daocloud.io/quay.io/containers/buildah:v1.35.4 \
     bash /code/build.sh
