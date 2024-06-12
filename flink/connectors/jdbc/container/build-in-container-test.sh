cf40481de0dfde2426172c3b67c1eef55238a270
ben.wangz <ben.wangz@foxmail.com>
2024-06-12 20:41:34 +0800

    bugfix: remove dot

--- flink/connectors/jdbc/container/build-in-container-test.sh
+++ flink/connectors/jdbc/container/build-in-container-test.sh
@@ -6,7 +6,7 @@ mkdir -p /tmp/build/containers
 mkdir -p /tmp/build/gradle
 podman run --rm \
     -v /tmp/build/containers:/var/lib/containers \
-    -v /tmp/build/.gradle:/root/.gradle \
+    -v /tmp/build/gradle:/root/.gradle \
     -v $SCRIPT_DIR/../../../..:/code \
     --privileged \
     -it m.daocloud.io/quay.io/containers/buildah:v1.35.4 \
