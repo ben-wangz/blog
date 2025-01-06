0de595cae50970e8d64ac0595dfa78efceb50399
ben.wangz <ben.wangz@foxmail.com>
2025-01-06 16:43:46 +0800

    bugfix

--- pytorch/classify-handwritten-digits/prediction.py
+++ pytorch/classify-handwritten-digits/prediction.py
@@ -6,6 +6,14 @@ from torch.utils.data import DataLoader
 from model import Net
 from model import ImageTransformer
 
+# Set the device according to the availability of CUDA
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print("using device: cuda")
+else:
+    device = torch.device('cpu')
+    print("using device: cpu")
+
 # Load and normalize the data
 transform = ImageTransformer()
 test_dataset = datasets.MNIST(
@@ -14,7 +22,7 @@ test_dataset = datasets.MNIST(
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 # Load the model
-model = Net()
+model = Net().to(device)
 model.load_state_dict(torch.load(".model/mnist.pth", weights_only=True))
 model.eval()  # Set the model to evaluation mode
 
@@ -24,8 +32,7 @@ images, labels = next(dataiter)
 
 img = images[0]  # Select one image
 img = img.unsqueeze(0)  # Add batch dimension
-if torch.cuda.is_available():
-    img = img.to("cuda")
+img = img.to(device)
 
 output = model(img)
 _, predicted = torch.max(output.data, 1)
